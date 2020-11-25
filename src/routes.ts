2d09ae2ba5001a23bdcc48c43740b1b46655bf59
Mariana <marianadealmeidapereira@outlook.com>
2020-11-25 18:48:23 -0300

    create auth middleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import express from 'express';
 import UserController from '@controllers/UserController';
 import SessionController from '@controllers/SessionController';
 
+import authMiddleware from './middlewares/authMiddleware';
+
 const routes = express.Router();
 
 routes.post('/session', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/users', UserController.index);
-routes.post('/users', UserController.store);
 routes.get('/users/:id', UserController.show);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
