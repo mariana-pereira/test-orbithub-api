6779737207a4a51dacf9834f0684c4d6790f454f
Mariana <marianadealmeidapereira@outlook.com>
2020-11-26 09:40:51 -0300

    add dotenv

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "type": "postgres",
-  "host": "localhost",
-  "port": 5432,
-  "username": "postgres",
-  "password": "docker",
-  "database": "testorbithub",
+  "host": process.env.TYPEORM_HOST,
+  "port": process.env.TYPEORM_PORT,
+  "username": process.env.TYPEORM_USERNAME,
+  "password": process.env.TYPEORM_PASSWORD,
+  "database": process.env.TYPEORM_DATABASE,
   "entities": [
     "./src/models/*.ts"
   ],
